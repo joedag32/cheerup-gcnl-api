8509cf2cb603c8de647d926d099988613483252f
Glitch (decorous-brash-cheque) <none>
2020-11-11 19:13:23 +0000

    🙉🚔 Checkpoint ./script.js:21103629/10940 ./index.html:21103629/24

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const functionURL =
   "https://eastus.api.cognitive.microsoft.com/text/analytics/v3.0/keyPhrases";
 // array of privacy terms that we are looking for a match on
-const privacyTerms = ['address', 'cookies', 'credit card', 'ip address', 'personal data', 'third-party'];
+const privacyTerms = [
+  "address",
+  "cookies",
+  "credit card",
+  "ip address",
+  "personal data",
+  "third-party"
+];
 const input = document.getElementById("testValue");
 const analyzeButton = document.getElementById("analyzeButton");
 const objectOutput = document.getElementById("returnedObject");
@@ -48,25 +55,33 @@ function analyzeText(e) {
 
 // evaluate the returend Key Phrases
 function evaluateKeyPhrases(data) {
+  objectOutput.innerHTML = "";
   let keyPhrasesArray = data.documents[0].keyPhrases;
-  // output phrases for web app demo
-  objectOutput.innerHTML += '<ul>';
-  keyPhrasesArray.forEach(element => objectOutput.innerHTML += '<li>' + element + '</li>');
-  objectOutput.innerHTML += '</ul>';
-  objectOutput.innerHTML += '<h2>Privacy Terms</h2>';
-  objectOutput.innerHTML += '<ul>';
-  privacyTerms.forEach(element => objectOutput.innerHTML += '<li>' + element + '</li>');
-  objectOutput.innerHTML += '</ul>';
-  
+
   // check to see if Key Phrases match any of our Privacy Terms
+  let privacyTermMatches = 0;
   keyPhrasesArray.forEach(phrase => {
     privacyTerms.forEach(term => {
       if (term.toLowerCase() === phrase.toLowerCase()) {
-        objectOutput.innerHTML += 'We have a match!!';
+        privacyTermMatches++;
       }
     });
   });
+  if (privacyTermMatches) {
+    objectOutput.innerHTML +=
+      "<h1>Matched Privacy Terms " + privacyTermMatches + " times!!</h1>";
+  }
 
-
-  //objectOutput.innerHTML = data.documents[0].keyPhrases;
+  // output phrases for web app demo
+  objectOutput.innerHTML += "<ul>";
+  keyPhrasesArray.forEach(
+    element => (objectOutput.innerHTML += "<li>" + element + "</li>")
+  );
+  objectOutput.innerHTML += "</ul>";
+  objectOutput.innerHTML += "<h2>Privacy Terms</h2>";
+  objectOutput.innerHTML += "<ul>";
+  privacyTerms.forEach(
+    element => (objectOutput.innerHTML += "<li>" + element + "</li>")
+  );
+  objectOutput.innerHTML += "</ul>";
 }
