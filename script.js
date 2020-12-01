8886d569b14fd8890a1c7c534e5117907192521a
Glitch (decorous-brash-cheque) <none>
2020-12-01 20:47:49 +0000

    🚵💨 Checkpoint ./script.js:21103629/2909 ./index.html:21103629/152

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* declare some variables */
-const apiKey = 'AIzaSyDkNZylSVJND4hX0fJFCPAi52JGU8e-Z4o';
-const functionURL = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + apiKey;
+const apiKey = "AIzaSyDkNZylSVJND4hX0fJFCPAi52JGU8e-Z4o";
+const functionURL =
+  "https://language.googleapis.com/v1/documents:analyzeSentiment?key=" + apiKey;
 
 const input = document.getElementById("testValue");
 const analyzeButton = document.getElementById("analyzeButton");
@@ -8,13 +9,13 @@ const objectOutput = document.getElementById("returnedObject");
 let testValue = input.value;
 /* set value of input field as the body to be sent via the POST request */
 const postData = {
-  documents: [
-    {
-      language: "en-us",
-      type: "PLAIN_TEXT",
-      content: testValue
-    }
-  ]
+  language: "en-us",
+  type: "PLAIN_TEXT",
+  content: testValue
+};
+const apiData = {
+  document: postData,
+  encodingType: "UTF8"
 };
 
 /* update value of input to pass */
@@ -22,18 +23,19 @@ input.addEventListener("input", updateValue);
 function updateValue(e) {
   let inputString = e.target.value;
   // trim text to 5000 characters to pass to Azure
-  postData.documents[0].content = inputString.substring(1,5000);
-  console.log('length: ' + postData.documents[0].content.length);
+  postData.content = inputString.substring(1, 5000);
+  console.log("length: " + postData.content.length);
 }
 
 analyzeButton.addEventListener("click", analyzeText);
 /* call Google Cloud Natural Language API */
 function analyzeText(e) {
-  console.log('hello');
+  console.log(apiData);
+  /*
   fetch(functionURL, {
     method: "POST",
     contentType: "application/json",
-    payload: JSON.stringify(postData)
+    payload: JSON.stringify(apiData)
   })
     .then(res => res.json())
     .then(data => {
@@ -42,4 +44,5 @@ function analyzeText(e) {
     .catch(error => {
       console.error(error);
     });
+    */
 }
