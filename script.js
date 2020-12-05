d1811c8bd80d6b73e19cfd0379bc2e367f4a8d56
Glitch (decorous-brash-cheque) <none>
2020-12-05 17:56:47 +0000

    🐇🚊 Checkpoint ./script.js:21103629/193 ./index.html:21103629/28 ./style.css:21103629/2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const positiveQuotes = [
   "Keep on trying!",
   "You almost got it!",
   "You’ve just about got it!",
-  "Nice going!",
+  "I!",
   "Smile!",
   "Remember, every cloud has a silver lining!",
   "You are valued!"
@@ -35,15 +35,11 @@ function updateValue(e) {
   let inputString = e.target.value;
   // trim text to 1000 characters before passing
   postData.content = inputString.substring(1, 1000);
-  console.log("length: " + postData.content.length);
 }
 
 analyzeButton.addEventListener("click", analyzeText);
 /* call Google Cloud Natural Language API */
 function analyzeText(e) {
-  console.log(JSON.stringify(apiData));
-  outputSentiment();
-/*
   fetch(functionURL, {
     method: "POST",
     contentType: "application/json",
@@ -51,16 +47,15 @@ function analyzeText(e) {
   })
     .then(res => res.json())
     .then(data => {
-      console.log(data);
       outputSentiment(data);
     })
     .catch(error => {
       console.error(error);
     });
-    */
 }
 
 function outputSentiment(data) {
+  objectOutput.innerHTML = '';
   // if sentiment score is less than 0 then output a random Quote, otherwise just confirm their positivity
   if (data.documentSentiment.score < 0) {
     let randomQuote = positiveQuotes[Math.floor(Math.random() * positiveQuotes.length)];
